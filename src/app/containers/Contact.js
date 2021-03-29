4ccf8de7e3350b864041dffea65feb653d753662
RyanMoss96 <rpmoss96@gmail.com>
2021-03-29 19:49:45 +0100

    Post request for form

--- src/app/containers/Contact.js
+++ src/app/containers/Contact.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Amplify, { API } from "aws-amplify";
 import aws_exports from "../../aws-exports";
 // import { withAuthenticator } from "aws-amplify-react";
@@ -7,11 +7,23 @@ Amplify.configure(aws_exports);
 
 
 function Contact() {
-    const submitContactForm = event => {
-        console.log('on click');
-        console.log(event);
 
-        API.get('portfolioSendEmail', '', {})
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const submitContactForm = event => {
+        event.preventDefault();
+
+        const contactMessage = {
+            body: {
+                name,
+                email,
+                message
+            }
+        };
+
+        API.post('portfolioApi', '', contactMessage)
             .then(response => console.log(response))
             .catch(error => console.log(error));
     };
@@ -24,15 +36,15 @@ function Contact() {
             </header>
             <div className='flex flex-col flex-wrap md:flex-row justify-between'>
                 <div className='bg-gray px-5 py-10 md:py-8 sm:p-8 my-2 shadow-lg w-full '>
-                    <form className='flex flex-col space-y-3 m-auto w-full' name='contact' method='post'>
+                    <form className='flex flex-col space-y-3 m-auto w-full' name='contact' onSubmit={submitContactForm}>
                         <input type='hidden' name='form-name' value='contact' />
                         <label htmlFor='name'>Name</label>
-                        <input type='text' name='name' id='name' className='gradient' required></input>
+                        <input type='text' name='name' id='name' className='gradient' onChange={event => setName(event.target.value)}  required></input>
                         <label htmlFor='email'>Email</label>
-                        <input type='email' name='email' id='email' className='gradient' required></input>
+                        <input type='email' name='email' id='email' className='gradient' onChange={event => setEmail(event.target.value)} required></input>
                         <label htmlFor='message'>Message</label>
-                        <textarea name='message' id='message' cols='25' rows='5' className='gradient' required></textarea>
-                        <button type='button' onClick={submitContactForm} className='border border-gray-500 p-2 rounded-lg w-auto mr-auto shadow-md'>
+                        <textarea name='message' id='message' cols='25' rows='5' className='gradient' onChange={event => setMessage(event.target.value)} required></textarea>
+                        <button type='submit' className='border border-gray-500 p-2 rounded-lg w-auto mr-auto shadow-md'>
                             Send Message
                         </button>
                     </form>
