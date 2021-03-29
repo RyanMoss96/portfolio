e4f6c80ba477816ca5051dd28d166347cc40e899
RyanMoss96 <rpmoss96@gmail.com>
2021-03-29 20:08:41 +0100

    Email lambda

--- amplify/backend/function/portfolioSendEmail/src/index.js
+++ amplify/backend/function/portfolioSendEmail/src/index.js
@@ -1,12 +1,31 @@
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
+var aws = require("aws-sdk");
+var ses = new aws.SES({ region: "eu-west-2" });
+
+exports.handler = async function (event) {
+  var params = {
+    Destination: {
+      ToAddresses: ["mail@ryanmoss.co.uk"],
+    },
+    Message: {
+      Body: {
+        Text: { Data: "Test" },
+      },
+
+      Subject: { Data: "Test Email" },
+    },
+    Source: "mail@ryanmoss.co.uk",
+  };
+
+  const email = ses.sendEmail(params).promise()
+
+  return email.then(result => {
+      return {
         statusCode: 200,
         headers: {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Headers": "*"
         },
-        body: JSON.stringify('Hello from Lambda!'),
+        body: event.body,
     };
-    return response;
+  })
 };
