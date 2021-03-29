3332b865da2bc90b2dc59fccb68f027217f22ca6
RyanMoss96 <rpmoss96@gmail.com>
2021-03-29 21:13:08 +0100

    Working email lambda

--- amplify/backend/function/portfolioSendEmail/src/index.js
+++ amplify/backend/function/portfolioSendEmail/src/index.js
@@ -1,13 +1,31 @@
+var aws = require("aws-sdk");
+var ses = new aws.SES({ region: "eu-west-2" });
+
 exports.handler = async (event) => {
-    // TODO implement
+    var params = {
+        Destination: {
+            ToAddresses: ["mail@ryanmoss.co.uk"],
+        },
+        Message: {
+            Body: {
+                Text: { Data: 
+                    `A new Contact Form Submission has been received by ${event.name} with the following email ${event.email} and message ${event.message}` },
+            },
+
+            Subject: { Data: `Contact Form Submission by ${event.name}` },
+        },
+        Source: "mail@ryanmoss.co.uk",
+    };
+
+    const result = await ses.sendEmail(params).promise()
+
     const response = {
         statusCode: 200,
         headers: {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Headers": "*"
         },
-        body: JSON.stringify('Hello from Lambda!'),
+        body: result,
     };
     return response;
 };
-
